feb8ae59894ef6a49176fcc3b46019e13e183972
MaxFalkenberg <mff113@ic.ac.uk>
2017-10-23 15:45:58 +0100

    Preliminary code for microtime updates in functional form

--- oslo_main.py
+++ oslo_main.py
@@ -88,7 +88,7 @@ class Oslo:
         else:
             return data[single]
 
-    def micro_run(self):
+    def micro_run(self,z,z_c):
         """Internal function.
             Executes all topplings of the ricepile at the macroscopic time
             the function is called. Records total number of toppings (avalance
@@ -97,76 +97,25 @@ class Oslo:
         tm = 0 #microscopic counter (not time!)
         sm = 0 #avalance index
         dm = 0 #drop index
-        boundary_topple = True #has the boundary site toppled at last microtime
+        check = [0]
+        continue_topple = True #has the boundary site toppled at last microtime
 
-        while boundary_topple:
-            boundary_topple = False
-            continue_topple = True #any sites left to topple
-            while continue_topple:
-                print(tm)
-                #Sites with possible toppings
-                print(self.index_choice[tm])
-                print(self.topple_check[tm])
-                dep_tm = self.index_choice[tm][self.topple_check[tm]]
-                z_dif = (self.__z[dep_tm] -
-                            self.__z_c[dep_tm])
-                #Sites that do topple
-                z_top = (z_dif + np.absolute(z_dif)).astype('bool')
-                top_sum = np.sum(z_top)
-                sm += top_sum
-                continue_topple = bool(top_sum)
-                if continue_topple: #Reset critical slopes at toppled sites
-                    self.__z_c[dep_tm[z_top]] = np.random.randint(1,2,top_sum)
-                    self.__z[dep_tm[z_top]] -= 2
-                    if tm == self.__L - 1:
-                        self.topple_check[tm-1] *= 0
-                        if (tm+1)%2:
-                            self.topple_check[tm-1] += z_top
-                            self.topple_check[tm-1][:-1] += z_top[1:]
-                            self.__z[dep_tm[z_top[1:]] - 1] += 1
-                        else:
-                            self.topple_check[tm-1] += z_top[1:]
-                            self.topple_check[tm-1] += z_top[:-1]
-                            self.__z[dep_tm[z_top] - 1] += 1
-                        if z_top[-1]:
-                            self.__z[-1] += 1
-                        tm -= 1
-                        boundary_topple = z_top[-1]
-                        dm += boundary_topple
-                    elif tm == self.__L - 2 and boundary_topple:
-                        if (tm+1)%2:
-                            self.topple_check[tm+1] += z_top
-                            self.topple_check[tm+1][:-1] += z_top[1:]
-                            self.topple_check[tm+1][-1] = True
-                            self.__z[dep_tm[z_top[1:]] - 1] += 1
-                            self.__z[dep_tm[z_top] + 1] += 1
-                        else:
-                            self.topple_check[tm+1][:-1] += z_top
-                            self.topple_check[tm+1][1:] += z_top
-                            self.topple_check[tm+1][-1] = True
-                            self.__z[dep_tm[z_top] - 1] += 1
-                            self.__z[dep_tm[z_top] + 1] += 1
-                        tm += 1
-                        continue_topple = True
-                        #Ensures boundary is checked again for double toppling
-                    else:
-                        if (tm+1)%2:
-                            self.topple_check[tm+1] += z_top
-                            self.topple_check[tm+1][:-1] += z_top[1:]
-                            self.__z[(dep_tm[z_top] - 1)[1:]] += 1
-                            self.__z[dep_tm[z_top] + 1] += 1
-                        else:
-                            self.topple_check[tm+1][:-1] += z_top
-                            self.topple_check[tm+1][1:] += z_top
-                            self.__z[dep_tm[z_top] - 1] += 1
-                            self.__z[dep_tm[z_top] + 1] += 1
-                        tm += 1
+        while continue_topple:
+            z_d = z[check] - z_c[check]
+            z_t = (z_d + np.absolute(z_d)).astype('bool')
+            sm_temp = np.sum(z_t)
+            z_c[z_t] = np.random.randint(1,2,sm_temp)
+            continue_topple = bool(sm_temp)
+            sm += sm_temp
+            if continue_topple:
+                check = self.update_check(self,z_t,tm)
+                z = self.update_z(self,z,z_t,tm)
 
         self.s.append(sm)
         self.d.append(dm)
 
-a = Oslo(10)
-b = np.ones(10,dtype='int')
-b[0] = 2
-a.custom_z(b)
-a.micro_run()
+    def update_check(self,z_t,tm):
+        """Docstring"""
+
+    def update_z(self,z,z_t,tm):
+        """Docstring"""
